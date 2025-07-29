ccea187a262a8c889ef313118e666bae3b1abf4b
OnFim25 <156911603+OnFim25@users.noreply.github.com>
2025-07-29 14:31:39 +0530

    Created project files

---
+++ Q43_OLED_interface/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: PC
+ *
+ * Created on 29 July, 2025, 2:29 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ Q43_OLED_interface/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Q43_OLED_interface.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Q43_OLED_interface.production.hex
